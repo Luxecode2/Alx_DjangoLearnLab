dd3b05fa2b4d3a3e770e21eb43308ad9682a9f1e
Luxecode2 <yvesalameen@gmail.com>
2025-09-07 22:32:30 +0100

    Add updated django-models project

--- myproject/book_store/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Welcome to my book store 📚")
-
-
-
-# Create your views here.
